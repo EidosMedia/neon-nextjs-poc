7e2108fdcedb64536b48570c2d46f54c4d93bb91
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-03-05 14:30:21 +0100

    prod resources base

--- cobalt.settings.js
+++ cobalt.settings.js
@@ -1,4 +1,5 @@
 // export const COBALT_BASE = 'http://express-website.em.corp:5443'
 // export const COBALT_PREVIEW_BASE = 'http://preview-express-website.em.corp:5443/'
 
-export const RESOURCES_PUBLIC_BASE = 'http://express-host.em.corp:5443'
+//export const RESOURCES_PUBLIC_BASE = 'http://express-host.em.corp:5443'
+export const RESOURCES_PUBLIC_BASE = 'https://cobalt-poc.demomedia.eidosmedia.io/'
