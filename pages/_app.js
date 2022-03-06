87f11049d65660755cc8beaa619a921effed0554
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-03-06 04:26:17 +0100

    simplest app

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,10 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from '../src/theme';
 
 import "react-image-gallery/styles/css/image-gallery.css";
 
-
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
-  return (
-    <React.Fragment>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
+
