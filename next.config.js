ba8cd56d760277ee04f6b1119b8b8487998110fc
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-03-06 04:06:57 +0100

    removed strict mode

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-const sites = [
-  {
-    siteKey: 'globe-main',
-    //domain: 'globe-main.vercel.app'
-    domain: 'globe-main.vercel.app'
-  },
-  {
-    siteKey: 'globe-italy',
-    //domain: 'globe-italy.vercel.app'
-    domain: 'globe-italy.vercel.app'
-  }
-]
-
 module.exports = {
-  reactStrictMode: true,
-  publicRuntimeConfig: {
-    sites
-  },
   images: {
     domains: ['demo.eidosmedia.io', 'express-website.em.corp', 'express-host.em.corp', 'cobalt-poc.demomedia.eidosmedia.io'],
     minimumCacheTTL: 3600
