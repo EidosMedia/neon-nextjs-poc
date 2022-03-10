bc84b688df35485fb09222f264e4208be7b69ecb
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-03-10 15:55:22 +0100

    cache update

--- apps.settings.js
+++ apps.settings.js
@@ -1,2 +1,2 @@
-export const COMMON_DATA_CACHE_TTL_SECONDS = 300;
+export const COMMON_DATA_CACHE_TTL_SECONDS = 60;
 export const COMMON_USE_NEXT_IMAGE = false;
--- pages/_sites/[site]/[[...url]].js
+++ pages/_sites/[site]/[[...url]].js
@@ -94,10 +94,6 @@ export async function getStaticProps({ params }) {
     console.log('RENDERING - site: ' + site + ' - path: ' + url);
 
     let cobaltData = await getCobaltPageByUrl(site, url);
-    // if (cobaltData.object.data.sys.baseType === 'section') {
-    //     console.log("Section page: performing a search instead")
-    //     cobaltData = await decorateSectionPageCobaltData(cobaltData)
-    // }
 
     let props = {
         cobaltData
