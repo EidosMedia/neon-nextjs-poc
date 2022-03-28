42ef3ba1f6b899aa4838c76ad46537170b3b6cab
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-03-28 19:58:27 +0200

    GA - pageviews log

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useEffect } from 'react'
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
@@ -6,6 +6,8 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import { useRouter } from 'next/router';
+import * as ga from '../src/lib/ga'
 
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -14,6 +16,23 @@ const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <CacheProvider value={emotionCache}>
