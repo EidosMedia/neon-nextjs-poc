1129bb2f1ff36e6183bb708c76efbc0bf6d1d91b
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-05-30 11:56:01 +0200

    preview cookie exp

--- pages/api/preview/[[...url]].js
+++ pages/api/preview/[[...url]].js
@@ -2,7 +2,7 @@ export default async (req, res) => {
     console.log("--- PREVIEW DATA ---")
     console.log(req.query)
     // Enable Preview Mode by setting the cookies
-    res.setPreviewData(req.query)
+    res.setPreviewData(req.query, {maxAge: 10})
     // Redirect to the path from the fetched post
     const redirectUrl = req.query.url.join('/');
     res.redirect('/'+redirectUrl)
