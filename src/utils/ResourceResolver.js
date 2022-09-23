4984cff7a23e12c3247be3f193a3dc2fd9eb63f2
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-09-23 11:15:35 +0200

    fix preview on query (2)

--- src/utils/ResourceResolver.js
+++ src/utils/ResourceResolver.js
@@ -5,6 +5,9 @@ export default function ResourceResolver(resourceUrl, previewData, site){
         url = encodeURIComponent(url)
         url = '/api/imageProxy?url='+ url
     } else {
+        if (site.includes("[PREVIEW]")){ // Manage the case in which a preview call returns live resources (e.g. queries in DWP)
+            site = site.split('[PREVIEW]')[0]
+        }
         url = process.env.NEXT_PUBLIC_RESOURCES_PUBLIC_BASE + resourceUrl + '?emk.site=' + site
     }
     return url 
