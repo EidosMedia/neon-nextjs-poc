3025da604d7cb497b85bb2f1953a027db66e9b76
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-10-25 12:35:22 +0200

    debug

--- pages/api/imageProxy.js
+++ pages/api/imageProxy.js
@@ -1,12 +1,13 @@
 export default async (req, res) => {
     const url = encodeURI(decodeURIComponent(req.query.url));
     const previewToken = url.substring(url.indexOf('token=')+6)
-
+    console.log(url)
     const result = await fetch(url, {
         headers: {
             'Cookie':"emk.previewDefaultContent=false; emk.previewToken="+previewToken+";"
         }
     });
+    console.log(result)
     const body = result.body;
     console.log(body)
     body.pipe(res);
