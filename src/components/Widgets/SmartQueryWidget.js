c0d20ee3798fcad596be281a1a377e6c2ed8fb8f
Francois Vermaut <francois.vermaut@eidosmedia.com>
2023-05-23 10:31:07 +0200

    fix prev commit

--- src/components/Widgets/SmartQueryWidget.js
+++ src/components/Widgets/SmartQueryWidget.js
@@ -1,7 +1,14 @@
+import { Box, Card, CardActionArea, CardContent, Container, Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import NextLink from 'next/link'
+import React from "react";
+
 export default function SmartQueryWidget({ cobaltData, semanticSearchData, gridContext }) {
     let render = null;
     const removeSimilars = (cobaltData.linkContext.linkData.parameters.removeSimilars === 'yes')
     const similarityThreshold = 0.9
+    
+    const boxTitle = cobaltData.linkContext.linkData.parameters.query
 
     let list = semanticSearchData.matches
     if (removeSimilars) {
@@ -17,15 +24,115 @@ export default function SmartQueryWidget({ cobaltData, semanticSearchData, gridC
     }
 
     if (list) {
+        // render = (
+        //     <div>This is a smart query:
+        //         <ul>
+        //             {list.map((el, i) => (
+        //                 <li>{el.score} - {el.metadata.headline}</li>
+        //             ))}
+        //         </ul>
+        //     </div>);
+        // render = (
+        //     <Grid container spacing={2}>
+        //         {list.map((object, i) => (
+        //             <Grid key={i} item xs={12} md={4}>
+        //                 <SmartQueryFragment data={object} includeSummary={true}/>
+        //             </Grid>
+        //         ))}
+        //     </Grid>
+        // )
+
         render = (
-            <div>This is a smart query:
-                <ul>
-                    {list.map((el, i) => (
-                        <li>{el.score} - {el.metadata.headline}</li>
+            <Container maxWidth="lg">
+                {(boxTitle ?
+                    <Box sx={{ mb: 2, backgroundColor:'secondary.main'}}
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center">
+                        <Typography sx={{ color: 'primary.main' }} variant="h4" component="h4">
+                            {boxTitle}
+                        </Typography>
+                    </Box> : null)}
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={8}>
+                        {list.slice(0, 1).map((object, i) => <SmartQueryFragment data={object} includeSummary={true} gridContext={{ xs: 12, md: 8 }}/>)}
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        {list.slice(1, 3).map((object, i) => <SmartQueryFragment data={object} includeSummary={true} gridContext={{ xs: 12, md: 4 }}/>)}
+                    </Grid>
+                    {list.slice(3).map((object, i) => (
+                        <Grid key={i} item xs={12} md={3}>
+                            <SmartQueryFragment data={object} includeSummary={true} gridContext={{ xs: 12, md: 3 }}/>
+                        </Grid>
                     ))}
-                </ul>
-            </div>);
+                </Grid>
+            </Container>
+        )
     }
+    return render;
+}
+
+function SmartQueryFragment({ data, includeSummary, gridContext }) {
+    let render = null;
+
+    let imageWidth = 800;
+    let imageHeight = 600;
+
+    const myUrl = "/";
+
+    let headlineVariant = "h4"
+
+    if (gridContext && (gridContext.md < 6)) {
+        imageWidth = 1024;
+        imageHeight = 576;
+    }
+    if (gridContext.md >= 6) {
+        headlineVariant = "h4"
+    } else if (gridContext.md >= 3) {
+        headlineVariant = "h6"
+    } else {
+        headlineVariant = "body1"
+    }
+
+    render = (
+        <React.Fragment>
+            <Card square elevation={0} sx={{ display: { xs: 'none', md: 'block' } }}>
+                <Image src={data.metadata.url} width={imageWidth} height={imageHeight} />
+                <NextLink href={myUrl}>
+                    <CardActionArea>
+                        <CardContent sx={{ py: 0, px: 0 }}>
+                            <Typography gutterBottom variant={headlineVariant} component="div">
+                                {data.metadata.headline}
+                            </Typography>
+                            {includeSummary ?
+                                <Typography sx={{ mb: 2 }} variant="body1" color="text.secondary">
+                                    {data.metadata.summary}
+                                </Typography>
+                                : null}
+                        </CardContent>
+                    </CardActionArea>
+                </NextLink>
+            </Card>
+            <Card square elevation={0} sx={{ display: { xs: 'block', md: 'none' }, borderBottom: 1, borderColor: 'grey.500' }}>
+                <Image src={data.metadata.url} width={imageWidth} height={imageHeight} />
+                <NextLink href={myUrl}>
+                    <CardActionArea>
+                        <CardContent sx={{ py: 0, px: 0 }}>
+                            <Typography gutterBottom variant={headlineVariant} component="div">
+                                {data.metadata.headline}
+                            </Typography>
+                            {includeSummary ?
+                                <Typography sx={{ mb: 2 }} variant="h6" color="text.secondary">
+                                    {data.metadata.summary}
+                                </Typography>
+                                : null}
+                        </CardContent>
+                    </CardActionArea>
+                </NextLink>
+            </Card>
+        </React.Fragment>
+    )
+
     return render;
 }
 
