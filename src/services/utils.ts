2b919ec8d6c19037631ac7a70e7ac60bd1667d74
Valerio Cassani <valerio.cassani@eidosmedia.com>
2024-10-17 15:23:46 +0200

    NEON: fixed url resolution

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,45 +1,39 @@
 import { NextRequest } from 'next/server';
 import { NeonConnection } from '@/NeonConnection/NeonConnection';
 
-export const getAPIHostname = async (
-  request: NextRequest,
-  viewStatus?: string | null
-) => {
-  const protocol = request.headers.get('x-forwarded-protocol') || 'http';
+export const getAPIHostname = async (request: NextRequest, viewStatus?: string | null) => {
+    const protocol = request.headers.get('x-forwarded-protocol') || 'http';
+    console.log('request.headers', request);
 
-  // calling its own host to use caching mechanism
-  const builtUrl = `${protocol}://${request.headers.get(
-    'x-forwarded-host'
-  )}/api/sites`;
+    // calling its own host to use caching mechanism
+    const builtUrl = `${process.env.BASE_NEON_FE_URL}/api/sites/live`;
 
-  const sitesResp = await fetch(builtUrl, {
-    // cache: 'no-cache',
-    next: { tags: ['sites'] },
-  });
+    console.log('builtUrl', builtUrl);
 
-  const sites = await sitesResp.json();
+    const sitesResp = await fetch(builtUrl, {
+        // cache: 'no-cache',
+        next: { tags: ['sites'] }
+    });
 
-  const siteFound = sites.find((site: any) =>
-    site.root.hostname.match(request.headers.get('x-forwarded-host'))
-  );
+    const sites = await sitesResp.json();
 
-  if (viewStatus === 'PREVIEW') {
-    return `${protocol}://${siteFound?.apiHostnames.previewHostname}`;
-  }
+    console.log('sites', sites);
 
-  return `${protocol}://${siteFound?.apiHostnames.liveHostname}`;
+    const siteFound = sites.find((site: any) => site.root.hostname.match(request.headers.get('x-forwarded-host')));
+
+    if (viewStatus === 'PREVIEW') {
+        return `${protocol}://${siteFound?.apiHostnames.previewHostname}`;
+    }
+
+    return `${protocol}://${siteFound?.apiHostnames.liveHostname}`;
 };
 
 export const getConnection = () => {
-  if (!process.env.BASE_NEON_FE_URL) {
-    throw new Error('BASE_NEON_FE_URL not specified in any .env file');
-  }
+    if (!process.env.BASE_NEON_FE_URL) {
+        throw new Error('BASE_NEON_FE_URL not specified in any .env file');
+    }
 
-  if (!globalThis.connection)
-    globalThis.connection = new NeonConnection(
-      process.env.BASE_NEON_FE_URL,
-      ''
-    );
+    if (!globalThis.connection) globalThis.connection = new NeonConnection(process.env.BASE_NEON_FE_URL, '');
 
-  return globalThis.connection;
+    return globalThis.connection;
 };
